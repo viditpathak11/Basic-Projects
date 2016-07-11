4d9bda6e1fe3a97bb409f95ce37ee095fc4ae918
viditpathak11 <viditpathak14@gmail.com>
2016-07-11 18:20:10 +0530

    Add files via upload

---
+++ Credit_Card_Validator.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int i;
+    double cardnumber;
+    printf("Give me a number: \n");
+    scanf("%lf", &cardnumber);
+
+    if(cardnumber < 1000000000000 || cardnumber > 10000000000000000)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+    if(cardnumber < 100000000000000 && cardnumber > 9999999999999)
+    {
+        printf("INVALID\n");
+        return 0;   
+    }
+
+    char creditcard[17];
+    sprintf(creditcard, "%f", cardnumber);
+    char* ptr_cc;
+    ptr_cc = strtok(creditcard,".");
+    int card_size = strlen(ptr_cc);
+    int sum = 0;
+    for( i = 1; i < card_size; i+=2)
+    {
+        int x = creditcard[card_size-1-i] - '0';
+        int prod = 2 * x;
+        if(prod>=10)
+        {
+            prod = prod%10 + prod/prod%10;
+        }
+        sum += prod;      
+    }
+    for(i = 0; i < card_size; i+=2)
+    {
+        int x = creditcard[card_size-1-i] - '0';
+        sum += x;      
+    }
+    if(sum%10 != 0)
+    {
+        printf("INVALID\n");
+        return 0; 
+    }
+    else if(creditcard[0] == '4')
+    {
+        printf("VISA\n");
+        return 0; 
+    }
+    else if(creditcard[0] == '3' && (creditcard[1] == '7' || creditcard[1] =='4'))
+    {
+        printf("AMEX\n");
+        return 0; 
+    }
+    else if(creditcard[0] == '5' && (creditcard[1] >='1' && creditcard[1] <='5'))
+    {
+        printf("MASTERCARD\n");
+        return 0; 
+    }
+    else
+    {
+        printf("INVALID\n");
+        return 0;    
+    }         
+}
---
+++ caesar.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // return 1 if user inputs improper # of command line arguments
+    if (argc != 2)
+    {
+        printf("Please enter a key!\n");
+        return 1;
+    }
+
+    //second command line argument is key
+    //convert key from string to int
+    int key = atoi(argv[1]);
+
+    //get string of text from user
+    printf("Kindly enter a plaintext:\n");
+    string s = GetString();
+    
+
+    //encrypt plain text using key
+    for (int i = 0, n = strlen(s); i < n; i++)
+        if (isalpha(s[i]))
+
+            //if capitalized
+            if (isupper(s[i]))
+            {
+                //keep ASCII value in uppercase range, rotate key places, print
+                s[i] = (s[i] - 'A' + key) % 26 + 'A';
+                printf("%c", s[i]);
+            }
+
+    //if lowercased
+            else 
+            {
+                //keep ASCII value in lowercase range, rotate key places, print
+                s[i] = (s[i] - 'a' + key) % 26 + 'a';
+                printf("%c", s[i]);
+            }
+
+    //if character is not part of the alaphabet
+        else
+        {
+            //print as is
+            printf("%c", s[i]);
+        }
+
+    //print new line
+    printf("\n");
+
+    //return 0 to indicate no errors occured
+    return 0;
+}
